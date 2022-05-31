03ee5cf043cb6e223fa4fc5d63775accb4dd9da9
Antonio Candioto <89643628+antoniolpcan@users.noreply.github.com>
2022-05-31 20:02:28 -0300

    Add files via upload

--- Codagem/Cadastro_PI2/PHP/ClassDatabase.php
+++ Codagem/Cadastro_PI2/PHP/ClassDatabase.php
@@ -31,7 +31,7 @@ class BancoDados{
 
     
     public function login_site($user, $pass){ 
-        $sql = "SELECT id FROM Usuario WHERE username = ('".$user."') AND ('".$pass ."')";
+        $sql = "SELECT id FROM Usuario WHERE username = ('".$user."') AND passw = ('".$pass ."')";
         if (mysqli_query($this->conn, $sql)) {
             $result = mysqli_query($this->conn, $sql);
             if (mysqli_num_rows($result) <= 0){
@@ -59,108 +59,6 @@ class BancoDados{
         } 
         else {
             echo "Error: " . $sql . "<br>" . mysqli_error($this->conn);
-        }
-    }
-
-    public function update_calculadora($soma, $id, $select){
-        $sql = "UPDATE Pontuacao SET resultado_calc = $soma + $select where id = $id";
-
-        if (mysqli_query($this->conn, $sql)) {
-            return $soma + $select;
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-    }
-
-    public function update_perfil($id, $nome, $contato, $cidade, $pais, $foto_perfil){
-        $sql = "UPDATE Usuario SET nome = '$nome', contato = '$contato', cidade = '$cidade', pais = '$pais', foto_perfil = '$foto_perfil'  where id = '$id';";
-        if (mysqli_query($this->conn, $sql)) {
-            echo "Atualização feita com sucesso";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-    }
-
-    public function update_arvore($id, $num_arvores){
-        $sql = "UPDATE Pontuacao SET arvores = $num_arvores where id = $id";
-
-        if (mysqli_query($this->conn, $sql)) {
-            echo "Atualização feita com sucesso";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-    }
-
-    public function icon_nivel($nivel){
-        if ($nivel >= 1) {
-            echo '<option value="wonderwoman_1">Mulher Maravilha</option>
-            <option value="superman_1">Super homem</option>';
-        } if ($nivel >= 5) {
-            echo '<option value="spider_man_1">Homem aranha</option>
-            <option value="flash_1">Flash</option>
-            <option value="green_greenlantern_1">Lanterna verde</option>';
-        } if ($nivel >= 10) {
-            echo '<option value="ironman_1">Homem de ferro</option>
-            <option value="capitain_america_1">Capitão América</option>';
-        } if ($nivel >= 15) {
-            echo '<option value="batman_1">Batman</option>
-            <option value="robin_1">Robin</option>';
-        } if ($nivel >= 20) {
-            echo '<option value="wonderwoman">Mulher Maravilha - Rosto</option>
-            <option value="superman">Super homem - Rosto</option>';
-        } if ($nivel >= 25) {
-            echo '<option value="wolverine">Wolverine</option>
-            <option value="thor">Thor</option>';
-        } if ($nivel >= 30) {
-            echo '<option value="spiderman">Homem aranha - Rosto</option>
-            <option value="deadpool">Deadpool - Rosto</option>';
-        }
-        if ($nivel >= 35) {
-            echo '<option value="batman">Batman - Rosto</option>';
-        }
-    }
-
-    public function conquistas_nivel($nivel){
-        if ($nivel >= 1) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center">Nova conta!</p>
-                </figure></div>';
-        } if ($nivel >= 5) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Nível 5! </p>
-                </figure></div>';
-
-        } if ($nivel >= 10) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Consciente! </p>
-                </figure></div>';
-
-        } if ($nivel >= 15) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Nível 15! </p>
-                </figure></div>';
-
-        } if ($nivel >= 25) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Vivendo e aprendendo! </p>
-                </figure></div>';
-
-        } if ($nivel >= 30) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Herói! </p>
-                </figure></div>';
-
-        } if ($nivel >= 35) {
-            echo '<div class="col"><figure class="figure">
-                    <img src="./images_perfil/medalha.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-                    <p class="text-center"> Nível 35! </p>
-                </figure></div>';
         }
     }
 
